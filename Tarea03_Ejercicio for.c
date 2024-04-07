c6bd5860f0f5301bab8a5a18be45b500392b37ab
CarlosManuel2505 <163787386+CarlosManuel2505@users.noreply.github.com>
2024-04-07 14:36:55 -0600

    Add files via upload

---
+++ Tarea03_Ejercicio for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	//Haciendo uso del ciclo for quiero imprimir las tablas de multiplicar, pero le
+	//	voy a solicitar al usuario que numero quiero para la tabla de multiplicar, una
+	//	vez dado el numero de la tabla, imprimir la tabla de multiplicar en consola
+	int i,n;
+	printf("Tablas de multiplicar.");
+	printf("\nIngresa el número de la tabla que desees: "); 
+	scanf("%d",&n);
+	printf("\nLa tabla de multiplicar del %d es: ",n);
+	for(i=1; i<=10; i++){
+		printf("\n %d x %d = %d",n,i,n*i);
+			}
+	
+	return 0;
+}
+
